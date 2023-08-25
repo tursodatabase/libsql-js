b6c38704aae96d05b2accc87bff9d5ed254db345
Pekka Enberg <penberg@iki.fi>
2023-08-25 19:39:29 +0300

    Make sync example use environment variables

--- examples/sync/example.js
+++ examples/sync/example.js
@@ -1,6 +1,9 @@
 import Database from "libsql-experimental";
 
-const options = { syncUrl: "http://localhost:8080" };
+const url = process.env.LIBSQL_URL;
+const authToken = process.env.LIBSQL_AUTH_TOKEN;
+
+const options = { syncUrl: url, syncAuth: authToken };
 const db = new Database("hello.db", options);
 
 db.sync();
