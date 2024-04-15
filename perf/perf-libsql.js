bce6517e32574d25e949e6202396d43529832f18
Pekka Enberg <penberg@iki.fi>
2024-04-15 20:20:09 +0300

    Add simple performance tests

---
+++ perf/perf-libsql.js
@@ -0,0 +1,26 @@
+import { run, bench, group, baseline } from 'mitata';
+
+import Database from 'libsql';
+
+const db = new Database(':memory:');
+
+db.exec("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)");
+db.exec("INSERT INTO users (id, name, email) VALUES (1, 'Alice', 'alice@example.org')");
+
+const stmt = db.prepare("SELECT * FROM users WHERE id = ?");
+
+group('Statement', () => {
+  bench('get(1)', () => {
+    stmt.get(1);
+  });
+});
+
+await run({
+  units: false, // print small units cheatsheet
+  silent: false, // enable/disable stdout output
+  avg: true, // enable/disable avg column (default: true)
+  json: false, // enable/disable json output (default: false)
+  colors: true, // enable/disable colors (default: true)
+  min_max: true, // enable/disable min/max column (default: true)
+  percentiles: true, // enable/disable percentiles column (default: true)
+});
