644ab4d4bc2e8e97b54cb2e2fc1a0cd4d39c2992
Pekka Enberg <penberg@turso.tech>
2025-03-20 11:08:07 +0200

    Remove todo! from throw_libsql_error()

--- src/errors.rs
+++ src/errors.rs
@@ -19,7 +19,6 @@ pub fn throw_libsql_error<'a, C: Context<'a>, T>(cx: &mut C, err: libsql::Error)
             cx.throw(err)?
         }
         _ => {
-            todo!("err = {:?}", err);
             let err = format!("{:?}", err);
             let err = JsError::error(cx, err).unwrap();
             let code = cx.string("");
